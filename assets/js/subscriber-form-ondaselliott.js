ec0d385fefa4ec652701b1335b5c5d81f068540a
w3bm4st3rfx <115816639+w3bm4st3rfx@users.noreply.github.com>
2025-04-21 19:42:01 -0300

    Update subscriber-form-ondaselliott.js

--- assets/js/subscriber-form-ondaselliott.js
+++ assets/js/subscriber-form-ondaselliott.js
@@ -1,13 +1,4 @@
-// Script para conectar HTML form a Firebase
-// Tutorial Youtube:  https://www.youtube.com/watch?v=RAWHXRTKTHw&t=1016s
-// Github: https://github.com/Vetrivel-VP/contact_form_firebase
-
-// EU, Fernando, ADICIONEI código para melhorar a validação do email.
-// Adicionei a funcionalidade de redirecionar após o email ser validado e enviado ao firebase
-
 const firebaseConfig = {
-    // Your web app's Firebase configuration
-    // Vc acha esses dados pra copiar-colar nas configurações de cada projeto    
   apiKey: "AIzaSyCAczT3X7zK6fsEvVNyjar1TU6FqZF1wQE",
   authDomain: "ondas-elliott.firebaseapp.com",
   databaseURL: "https://ondas-elliott-default-rtdb.firebaseio.com",
@@ -19,10 +10,8 @@ const firebaseConfig = {
 
 
 
-// initialize firebase
 firebase.initializeApp(firebaseConfig);
 
-// reference your database
 var contactFormDB = firebase.database().ref("contactForm");
 
 document.getElementById("contactForm").addEventListener("submit", submitForm);
@@ -35,35 +24,20 @@ function submitForm(e) {
   const redirectUrl = "https://radarbolsa.com/assinaturas/sucesso-dt.html";  
 
 
-// Se o email digitado for válido...
   if (validateEmail(emailid)) {
         
 	  saveMessages(emailid);
 
-      // redirecionamento após enviar dados para o Firebase
         window.location.href = redirectUrl;
               
-	  // Eu desabilitei essa funcionalidade.
-	  /*  enable alert
-	  document.querySelector(".alert").style.display = "block";
 
-	  //   remove the alert after 3 secs
-	  setTimeout(() => {
-		document.querySelector(".alert").style.display = "none";
-	  }, 3000);
-
-	  //   reset the form
-	  document.getElementById("contactForm").reset(); 
-	  */
  }
  
  else {
 	 
-   // msg DE ERRO
 	 errorMessage.style.display = 'block';
      console.log("Email is invalid");
 
-   //   remove a msg de Erro após 3 segs
 	  setTimeout(() => {
 		errorMessage.style.display = "none";
 	  }, 3000);
@@ -75,7 +49,6 @@ function submitForm(e) {
 }
 
  
- // Função  de validação do email via REGEX 
   function validateEmail(emailid) {
       const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       return regex.test(emailid);
